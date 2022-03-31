6ef78968eed33e74467c02a7ebd481ca6e06d9cc
Администратор <@WIN-GMAKK9MSC73>
2022-03-31 08:21:11 +0300

    Ex2

---
+++ src/HW2/SimpleCalculator/TestCalc.java
@@ -0,0 +1,12 @@
+package HW2.SimpleCalculator;
+
+public class TestCalc {
+    public static void main(String[] args) {
+        Calc.sum(2,3);
+        Calc.div(5.0,2.2);
+        Calc.mult(3,3);
+        Calc.sub(100,3);
+
+
+    }
+}
