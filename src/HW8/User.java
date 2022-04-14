c62fd533db7ad433f6590d1b01d719437c1af845
Администратор <@WIN-GMAKK9MSC73>
2022-04-14 15:00:02 +0300

    lesson 8

---
+++ src/HW8/User.java
@@ -0,0 +1,19 @@
+package HW8;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
